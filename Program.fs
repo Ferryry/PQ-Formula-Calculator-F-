ea9e3e8c0bd34bce3b1b512a63587a440ff62933
Riley Schneider <88947587+Ferryry@users.noreply.github.com>
2021-08-22 19:15:45 +0200

    Update Program.fs

--- Program.fs
+++ Program.fs
@@ -1,28 +1,41 @@
-open System
+namespace PQTool
 
-let read_key() : double =
-    let ok, result = Double.TryParse(Console.ReadLine())
-    if ok then
-        result
-    else
-        raise(Exception("Use numbers only"))
+open Elmish
+open Avalonia
+open Avalonia.FuncUI.Components.Hosts
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.FuncUI
 
-let PQ p q =
-    if (Math.Pow(p/2.0, 2.0) - q) < 0.0 then
-        raise(Exception("Error"))
+type MainWindow() as this =
+    inherit HostWindow()
 
-    let temp = [| -(p/2.0) + Math.Sqrt(Math.Pow(p/2.0, 2.0) - q), -(p/2.0) - Math.Sqrt(Math.Pow(p/2.0, 2.0) - q)|]
-    temp
+    do
+        base.Title <- "PQ Formula Calculator"
+        base.Width <- 600.0
+        base.Height <- 400.0
+        base.Background <- Media.Brush.Parse("white")
+        base.CanResize <- false
+        
+        Elmish.Program.mkSimple (fun() -> Main.init) Main.Update Main.View
+        |> Avalonia.FuncUI.Elmish.Program.withHost this
+        |> Program.run
+        
+type App() =
+    inherit Application()
 
-[<EntryPoint>]
-let main argv =
-    printf "Value for P: "
-    let p1 : double = read_key()
+    override this.Initialize() =
+        this.Styles.Load "avares://Avalonia.Themes.Default/DefaultTheme.xaml"
+        this.Styles.Load "avares://Avalonia.Themes.Default/Accents/BaseDark.xaml"
 
-    printf "Value for Q: "
-    let q1 : double = read_key()
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            desktopLifetime.MainWindow <- MainWindow()
 
-    let vol = PQ p1 q1
-    printf "\nf(x) = x² + p + q\n"
-    printf "x1 = %A; x2 = %A" (fst vol.[0]) (snd vol.[0])
-    0
+        | _ -> ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main(args: string[]) =
+        AppBuilder.Configure<App>().UsePlatformDetect().UseSkia().StartWithClassicDesktopLifetime(args)
